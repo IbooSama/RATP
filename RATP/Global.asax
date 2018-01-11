ade0a63d42b85d987e5bfe41c067a01b8aea8318
cvernerie <verneriecamille@gmail.com>
2018-01-11 18:57:15 +0100

    add files

---
+++ RATP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Inherits="RATP.Global" %>
---
+++ RATP/Models/Line.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace RATP.Models
+{
+    public class Line
+    {
+        private string name;
+
+        private int id;
+
+        private int code;
+
+        private string directions;
+
+        public Line(String name, int id, int code, string directions)
+        {
+            this.name = name;
+            this.id = id;
+            this.code = code;
+            this.directions = directions;
+        }
+    }
+}
