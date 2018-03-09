b91dea05ff90a6617a3cf9860f247a50e9228300
cvernerie <verneriecamille@gmail.com>
2018-03-09 19:15:57 +0100

    improve front display

--- RATP/Models/SeedData.cs
+++ RATP/Models/SeedData.cs
@@ -2,11 +2,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
-using System.Collections.Generic;
 
 namespace RATP.Models
 {
