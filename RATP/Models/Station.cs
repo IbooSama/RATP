c592a056982b336e024057caeda5c665c9f099a4
cvernerie <verneriecamille@gmail.com>
2018-03-11 20:02:45 +0100

    add number in station model in order to keep track of the stations order on the line

--- RATP/Models/Station.cs
+++ RATP/Models/Station.cs
@@ -10,6 +10,7 @@ namespace RATP.Models
         public string Name { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
+        public int Number { get; set; }
 
         public Station()
         {
